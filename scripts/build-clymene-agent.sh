131071fa10e2c60a59197bb7665a1302b218ffa8
bourbonkk <pig3653@gmail.com>
2021-12-04 19:11:03 +0900

    setting ci env

--- scripts/build-clymene-agent.sh
+++ scripts/build-clymene-agent.sh
@@ -2,12 +2,12 @@
 
 BRANCH=${BRANCH:?'missing BRANCH env var'}
 GOARCH=${GOARCH:-$(go env GOARCH)}
-#GIT_SHA=$(shell git rev-parse HEAD)
+GIT_SHA=$(shell git rev-parse HEAD)
 #DATE=$(shell date -u +'%Y-%m-%dT%H:%M:%SZ')
 
 mkdir ./out
 
-go build -ldflags "-X 'main.branch=${BRANCH}" -o ./out/clymene-agent ./cmd/agent/main.go
+go build -ldflags "-X 'main.Version=${BRANCH}(${GIT_SHA})' -X 'main.BuildTime=${DATE}" -o ./out/clymene-agent ./cmd/agent/main.go
 
 docker build -f ../cmd/agent/Dockerfile -t bourbonkk/clymene:"${BRANCH}"
 docker push bourbonkk/clymene:"${BRANCH}"
